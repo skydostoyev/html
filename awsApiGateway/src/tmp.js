c2c55adc99f6252c6b714bd12f3e5eac0e7348db
skyDostoev <*****@users.noreply.github.com>
2022-11-08 16:32:43 +0900

    a

--- awsApiGateway/src/tmp.js
+++ awsApiGateway/src/tmp.js
@@ -1,2 +1,27 @@
-jwt = require("jsonwebtoken");
-console.log("test"+jwt);
+const AWS = require('aws-sdk'); 
+const nodeMailer = require("nodemailer")
+exports.sendMail = async(from, to, totp6) => {
+  
+   const transporter = nodeMailer.createTransport({
+     host: "smtp.host.com",
+     secure: true,
+     port: 465,
+     auth: {
+       user: "user",
+       pass: "pass",
+     },
+   });
+   const mailOptions = {
+      from: from, // sender address
+      to: to,
+      subject: "subject", // Subject line
+      html: `<p>Your passcode is 123455.</p>`, // plain text body
+     };
+     return await transporter.sendMail(mailOptions, function(err, info) {
+        if (err) { 
+           return "Error, Please try again."
+         }else{
+            return "OK, Please check the passcode in the mail."
+         }
+     }).promise()
+ 　}
