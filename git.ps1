1b4e3bcabd35d2955f72a8c2aa1f85f15266d7c2
skyDostoev <*****@users.noreply.github.com>
2023-01-02 00:33:09 +0900

    01/02/2023 09:33:09

--- git.ps1
+++ git.ps1
@@ -1,6 +1,16 @@
+$branch = authUsingEmail
+$tm = Get-Date (Get-Date).AddHours(+9)
+$arr =@(
+    "git add ." ,  
+    "git commit -am '$tm'",
+    "git push -u origin $branch"
+) 
 try{
-   Invoke-Expression "git add ." 
-   $tm = Get-Date (Get-Date).AddHours(+9)
-   Invoke-Expression "git commit -am '$tm'"
-   Invoke-Expression "git push -u origin authUsingEmail"
+   foreach($e in $arr){
+      Invoke-Expression $e   
+   }
+   # Invoke-Expression "git add ." 
+   # $tm = Get-Date (Get-Date).AddHours(+9)
+   # Invoke-Expression "git commit -am '$tm'"
+   # Invoke-Expression "git push -u origin authUsingEmail"
 }catch{Write-Host $_.Exception.Message}
